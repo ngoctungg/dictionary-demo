e1be714aacee7c874399f639ee246cc7452906d5
ngoctungg <m.ngoctung@gmail.com>
2020-10-20 20:03:26 +0700

    add test repositories

---
+++ src/test/java/com/pm/repository/FileRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.pm.repository;
+
+import com.pm.entity.CategoryEntity;
+import com.pm.entity.FileEntity;
+import com.pm.entity.PostEntity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileRepositoryTest {
+
+    @Autowired
+    private FileRepository fileRepository;
+
+    @Test
+    public void insert() throws Exception {
+        FileEntity fileEntity = new FileEntity();
+        fileEntity.setName("sdfsdf");
+        fileEntity.setPath("sdfsdf/90");
+
+        PostEntity postEntity = new PostEntity();
+        postEntity.setId(1);
+        fileEntity.setPost(postEntity);
+
+        fileRepository.save(fileEntity);
+        assertNotNull(fileEntity.getId());
+    }
+
+
+}
---
+++ src/test/java/com/pm/repository/UserRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.pm.repository;
+
+import com.pm.ProjectManagerApplication;
+import com.pm.entity.RoleEntity;
+import com.pm.entity.UserEntity;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testJoin() throws Exception{
+        UserEntity userEntity = new UserEntity();
+        userEntity.setAccount("tungmn");
+        userEntity.setPassword("hello");
+
+        RoleEntity roleEntity = new RoleEntity();
+        roleEntity.setName("user");
+
+        assertNull(userEntity.getId());
+        assertNull(roleEntity.getId());
+
+        userEntity.setRoles(Arrays.asList(roleEntity));
+        roleEntity.setUsers(Arrays.asList(userEntity));
+
+        userRepository.saveAndFlush(userEntity);
+
+        assertNotNull(userEntity.getId());
+        assertNotNull(roleEntity.getId());
+    }
+
+    @Test
+    @Transactional
+    public void selectJoinUserAndRole(){
+        UserEntity user = userRepository.findAll().get(0);
+        assertNotNull(user);
+        assertEquals(user.getRoles().size(), 1);
+        assertEquals("user",user.getRoles().get(0).getName().trim());
+    }
+
+
+}
