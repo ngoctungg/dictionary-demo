a908ebbe163e4b79c89bd64237df9197d77a874f
ngoctungg <m.ngoctung@gmail.com>
2020-10-24 15:02:38 +0700

    improve performance query home page

--- src/main/java/com/pm/repository/CategoryRepository.java
+++ src/main/java/com/pm/repository/CategoryRepository.java
@@ -1,7 +1,15 @@
 package com.pm.repository;
 
 import com.pm.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+
+    @Query("SELECT c FROM CategoryEntity c JOIN c.posts")
+    @EntityGraph(attributePaths = {"posts"})
+    List<CategoryEntity> findAllCategoriesAndPost();
 }
