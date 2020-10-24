52db211c3cf4e66712dc2ebdd95800b745ee18a2
ngoctungg <m.ngoctung@gmail.com>
2020-10-24 15:03:09 +0700

    handle exception

--- src/main/java/com/pm/model/ResponseMessage.java
+++ src/main/java/com/pm/model/ResponseMessage.java
@@ -1,8 +1,10 @@
 package com.pm.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true )
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseMessage <T>{
     private String status;
     private String msg;
