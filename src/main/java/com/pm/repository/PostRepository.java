44f390fbfa30e2632c0f2278cf8c1c1c6073704c
ngoctungg <m.ngoctung@gmail.com>
2020-10-25 23:44:11 +0700

    implement full text search api and change db

--- src/main/java/com/pm/repository/PostRepository.java
+++ src/main/java/com/pm/repository/PostRepository.java
@@ -2,6 +2,13 @@ package com.pm.repository;
 
 import com.pm.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+    @Query(value = "SELECT id,title,summary,category_id,null as content, MATCH(content) AGAINST (:keyword) as score FROM post " +
+            "WHERE MATCH(content) AGAINST (:keyword) > :accuracy order by score desc", nativeQuery = true)
+    List<PostEntity> searchPost(@Param("keyword") String q, @Param("accuracy") double accuracy);
 }
