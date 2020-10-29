16ae373c0c1549cb491e8c9ae26a83eea62c65ea
ngoctungg <m.ngoctung@gmail.com>
2020-10-29 16:23:29 +0700

    implement auditor

--- src/main/java/com/pm/audit/EntityAuditorAware.java
+++ src/main/java/com/pm/audit/EntityAuditorAware.java
@@ -1,13 +1,21 @@
 package com.pm.audit;
 
 import com.pm.entity.UserEntity;
+import com.pm.model.CustomUserDetails;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 public class EntityAuditorAware implements AuditorAware<UserEntity> {
     @Override
     public Optional<UserEntity> getCurrentAuditor() {
-        return Optional.of(new UserEntity(1));
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        return Optional.of(customUserDetails.getUser());
     }
 
 }
